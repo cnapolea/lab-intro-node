06d7f0648aac07d57632470bbac9eb647f07fe4a
cnapolea <cjnapoleao@gmail.com>
2021-08-11 22:46:37 +0100

    complete iteration 1-3)

--- index.js
+++ index.js
@@ -1,9 +1,23 @@
+// jshint esversion:10
+
 class SortedList {
-  constructor() {}
+  constructor(items) {
+    this.items = Array.isArray(items) ? items : [];
+    this.length = this.items.length;
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item);
+    this.items.sort((first, second) => {
+      if (first < second) return -1;
+      else if (first > second) return 1;
+    });
+  }
 
-  get(pos) {}
+  get(pos) {
+    if (!typeof this.items.at(pos)) throw new Error('OutOfBounds');
+    else return this.items.at(pos);
+  }
 
   max() {}
 
@@ -13,5 +27,12 @@ class SortedList {
 
   avg() {}
 }
+
+const myList = new SortedList([3, 1, 4, 2, 7]);
+myList.add(10);
+let value = myList.get(1);
+
+console.log(myList.items);
+console.log(value);
 
 module.exports = SortedList;
